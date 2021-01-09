61c80820fd197d85a57e45fa5990c480e715a249
RafaelSdm <RafaelSdm@users.noreply.github.com>
2021-01-09 09:25:18 -0300

    Create while.cpp

---
+++ C++/while.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n =0;
+
+    while (n <=100)
+    {
+        cout<< n <<
+        
+        n++;
+        /* code */
+    }
+    
+    
+    return 0;
+}
